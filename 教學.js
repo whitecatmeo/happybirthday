79a7d72764c3d0087bfb5bc5cc2f280f5f3c2320
whitecatmeo <122612581+whitecatmeo@users.noreply.github.com>
2023-04-15 21:06:05 +0800

    Update 教學.js

--- 教學.js
+++ 教學.js
@@ -3,7 +3,7 @@ function bb() {
 }
 
 function cc() {
-    location.href=("https://whitecatmeo.github.io/catcatmeowmeow/%E7%B5%A6%E5%B0%8F%E7%BE%8A%E7%BE%8A%E7%9A%84%E8%A9%B1.html")
+    location.href=("https://whitecatmeo.github.io/happybirthday/%E5%91%8A%E7%99%BD%E6%96%87")
 }
 
 /*
